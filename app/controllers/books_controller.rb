497ea808b9c02047777b6edbd1670a824d0278ff
terakawaakari <terakawaakari0701@gmail.com>
2021-04-21 15:39:40 +0000

    [Add]edit,update,destroy機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,13 +25,19 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
