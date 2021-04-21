aab6e0221aa4068c14d1d2dc35a2cdb9dae97ebb
terakawaakari <terakawaakari0701@gmail.com>
2021-04-21 09:36:56 +0000

    [Add]top,indexページ新規投稿・保存機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,37 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    book = Book.find(params[:id])
+    book.update
+    redirect_to books_path
+  end
+
+  def update
+
+  end
+
+
+  private
+  def book_params
+    params.permit(:title, :body)
+  end
+
 end
