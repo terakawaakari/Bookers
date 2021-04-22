dfbe1ee17aa3418ff49213cb5f43fec1ed02340e
terakawaakari <terakawaakari0701@gmail.com>
2021-04-22 08:37:13 +0000

    [Add]レイアウトの調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,6 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-  end
-
-  def new
     @book = Book.new
   end
 
@@ -27,7 +24,7 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
