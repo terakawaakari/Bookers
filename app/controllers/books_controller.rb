3701c45ef5503cfe8b12686d98e932fca663ff63
terakawaakari <terakawaakari0701@gmail.com>
2021-04-22 15:32:33 +0000

    [Add]データ編集時のバリデーション機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,8 +25,8 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       redirect_to book_path(book)
       flash[:success] = "Book was successfully updated."
     else
